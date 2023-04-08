ee66fd27ffc5fa6ad269fa5b1a9ef5cf80fed1a8
Evenmore <evenmore@Evenmores-MacBook-Pro.local>
2023-04-08 16:35:54 +0530

    add mongo uri

--- Config/Database.js
+++ Config/Database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 module.exports = function () {
     mongoose.set("strictQuery", false);
-    mongoose.connect("mongodb://dhruvik:Dhruvik@123@cluster0.2igwqig.mongodb.net/hrms?retryWrites=true&w=majority", { useNewUrlParser: true });
+    mongoose.connect("mongodb+srv://dhruvik:dhruvik@cluster0.2igwqig.mongodb.net/hrms?retryWrites=true&w=majority", { useNewUrlParser: true });
     // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
     mongoose.connection.on("connected", () => {
--- Routers/AuthRoute.js
+++ Routers/AuthRoute.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../Controllers/AuthController');
 
+router.get("/", (req, res) => {
+    res.send("Welcome To Hrms")
+})
+
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.post('/logout', AuthController.logout);
