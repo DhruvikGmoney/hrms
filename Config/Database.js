3730658cc6af2bf55a268d5b100687453fdf8a2d
Evenmore <evenmore@Evenmores-MacBook-Pro.local>
2023-04-08 16:17:47 +0530

    add mongo uri

--- Config/Database.js
+++ Config/Database.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv').config();
 
 module.exports = function () {
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+    mongoose.connect("mongodb://dhruvik:Dhruvik@123@cluster0.2igwqig.mongodb.net/hrms?retryWrites=true&w=majority", { useNewUrlParser: true });
+    // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
     mongoose.connection.on("connected", () => {
         console.log("Database Successfully Connected");
