9c716ac5c2ad4d73c8cd8583eda67e4ce2e8fb47
Evenmore <evenmore@Evenmores-MacBook-Pro.local>
2023-04-08 16:12:23 +0530

    test

--- Routers/AttendanceRoute.js
+++ Routers/AttendanceRoute.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const authorize = require('../middleware/auth');
+// const authorize = require('../middleware/auth');
 const AttendanceController = require('../Controllers/AttendanceController');
 const Role = require('../Helpers/role')
 
-router.post('/addAttendance',authorize([Role.STAFF, Role.ADMIN]), AttendanceController.addAttendance);
-router.get('/getAllAttendance',authorize, AttendanceController.getAllAttendance);
-router.get('/getAttendanceById/:attendance_id',authorize, AttendanceController.getAttendanceById);
-router.put('/updateAttendance?:attendance_id',authorize, AttendanceController.updateAttendance);
-router.delete('/deleteAttendance/:attendance_id',authorize, AttendanceController.deleteAttendance);
+// router.post('/addAttendance',authorize([Role.STAFF, Role.ADMIN]), AttendanceController.addAttendance);
+// router.get('/getAllAttendance',authorize, AttendanceController.getAllAttendance);
+// router.get('/getAttendanceById/:attendance_id',authorize, AttendanceController.getAttendanceById);
+// router.put('/updateAttendance?:attendance_id',authorize, AttendanceController.updateAttendance);
+// router.delete('/deleteAttendance/:attendance_id',authorize, AttendanceController.deleteAttendance);
+
+
+router.post('/addAttendance', AttendanceController.addAttendance);
+router.get('/getAllAttendance', AttendanceController.getAllAttendance);
+router.get('/getAttendanceById/:attendance_id', AttendanceController.getAttendanceById);
+router.put('/updateAttendance?:attendance_id', AttendanceController.updateAttendance);
+router.delete('/deleteAttendance/:attendance_id', AttendanceController.deleteAttendance);
+
 
 module.exports = router;
