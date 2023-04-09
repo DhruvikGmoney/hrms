1fca04a8268335563acc2ccd37564e5bc57fb4ef
Evenmore <evenmore@Evenmores-MacBook-Pro.local>
2023-04-09 09:31:01 +0530

    change for env

--- Config/Database.js
+++ Config/Database.js
@@ -3,8 +3,7 @@ const dotenv = require('dotenv').config();
 
 module.exports = function () {
     mongoose.set("strictQuery", false);
-    mongoose.connect("mongodb+srv://dhruvik:dhruvik@cluster0.2igwqig.mongodb.net/hrms?retryWrites=true&w=majority", { useNewUrlParser: true });
-    // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
     mongoose.connection.on("connected", () => {
         console.log("Database Successfully Connected");
